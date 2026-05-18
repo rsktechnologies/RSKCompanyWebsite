d0f156e9b81d217a41d4ba7837144b7ffbe3460e
Desire <asinguradesirecomfort@gmail.com>
2026-05-18 13:01:12 +0300

    fix: lint

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',   // <-- ADD THIS LINE
+  output: 'export', 
   images: {
     unoptimized: true,
   },
